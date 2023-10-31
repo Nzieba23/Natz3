3725e14409445e7ed3d4999b80badc64935efc0e
NatanZ <kapusta@kapusta.kapusta>
2023-10-31 09:52:37 +0100

    Initial commit

---
+++ src/NajlepszeGry.java
@@ -0,0 +1,12 @@
+public class NajlepszeGry {
+    public static void main(String[] args) {
+        System.out.println("Według mnie najlepsze gry to: ");
+        System.out.print("\"Minecraft \" (bo napisany w Javie:)");
+        System.out.print ("\" WOT \"");
+        System.out.print("\"CS 1.6\"");
+        System.out.println("\"Dying Light\"");
+        System.out.println("A najgorsze gry to: ");
+        System.out.print("\"League of legends\"");
+        System.out.print("\"Roblox\"");
+    }
+}
---
+++ src/zadanie2.java
@@ -0,0 +1,7 @@
+public class zadanie2 {
+    public static void main(String[] args) {
+        System.out.println("Natan Zięba");
+        System.out.println("1 Brygady 12/47");
+        System.out.println("");
+    }
+}
